26e7e8a911f30c979079a0f1706dea1f41de52f0
elcr <elcr@outlook.com>
2020-05-23 00:25:42 +0100

    index controller

--- src/controllers/IndexController.bs.js
+++ src/controllers/IndexController.bs.js
@@ -2,13 +2,40 @@
 'use strict';
 
 var Block = require("bs-platform/lib/js/block.js");
+var Curry = require("bs-platform/lib/js/curry.js");
+var Constants = require("../Constants.bs.js");
+var Relude_IO = require("relude/src/Relude_IO.bs.js");
+var Caml_int32 = require("bs-platform/lib/js/caml_int32.js");
+var Relude_Int = require("relude/src/Relude_Int.bs.js");
+var Relude_List = require("relude/src/Relude_List.bs.js");
+var Relude_Option = require("relude/src/Relude_Option.bs.js");
 
 function makeResponse(entries, page) {
-  return /* Pure */Block.__(0, [/* Page */Block.__(0, [
-                /* data */undefined,
-                /* status */200
-              ])]);
+  return Relude_IO.fromOption((function (prim) {
+                
+              }), Curry._2(Relude_Option.flatMap, (function (param) {
+                    return Relude_Option.map((function (pageEntries) {
+                                  return /* Page */Block.__(0, [
+                                            /* data *//* Index */Block.__(0, [{
+                                                  page: page,
+                                                  total: Curry._1(Relude_List.length, entries),
+                                                  entries: Relude_List.map((function (entry) {
+                                                            return {
+                                                                    title: entry.title,
+                                                                    date: entry.date,
+                                                                    text: entry.text
+                                                                  };
+                                                          }))(pageEntries)
+                                                }]),
+                                            /* status */200
+                                          ]);
+                                }), Relude_Option.filter((function (pageEntries) {
+                                        return Curry._1(Relude_List.length, pageEntries) !== 0;
+                                      }))(Relude_List.take(Constants.entriesPerPage, param[1])));
+                  }), Relude_List.splitAt(Caml_int32.imul(page - 1 | 0, Constants.entriesPerPage), Relude_List.sortBy((function (a, b) {
+                            return Curry._2(Relude_Int.compare, b.date.getTime(), a.date.getTime());
+                          }), entries))));
 }
 
 exports.makeResponse = makeResponse;
-/* No side effect */
+/* Relude_IO Not a pure module */
--- src/controllers/IndexController.re
+++ src/controllers/IndexController.re
@@ -2,5 +2,34 @@ open Relude.Globals;
 
 
 let makeResponse = (~entries, ~page) =>
-    Response.Page({ data: None, status: 200 })
-        |. IO.Pure;
+    entries
+        |> List.sortBy((a: Parse.parsedEntry, b) =>
+            Int.compare(
+                Date.toUnixTimestamp(b.date),
+                Date.toUnixTimestamp(a.date)
+            )
+        )
+        |> List.splitAt((page - 1) * Constants.entriesPerPage)
+        |> Option.flatMap(((_, tail)) =>
+            List.take(Constants.entriesPerPage, tail)
+                |. Some
+                |> Option.filter(pageEntries => List.length(pageEntries) !== 0)
+                |> Option.map(pageEntries =>
+                    Response.Page({
+                        status: 200,
+                        data: Some(Index({
+                            page,
+                            total: List.length(entries),
+                            entries: List.map(
+                                (entry: Parse.parsedEntry) => PageData.{
+                                    title: entry.title,
+                                    date: entry.date,
+                                    text: entry.text
+                                },
+                                pageEntries
+                            )
+                        }))
+                    })
+                )
+        )
+        |> IO.fromOption(ignore);
